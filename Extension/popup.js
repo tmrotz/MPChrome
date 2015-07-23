2a725de702b0fecf76e105f09603d48740483568
Travis Rotz <tmrotz@gmail.com>
2015-07-23 00:06:56 -0600

    Clean up, and use JQuery

--- Extension/popup.js
+++ Extension/popup.js
@@ -9,44 +9,26 @@
  *   is found.
  */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
+
   var queryInfo = {
     active: true,
     currentWindow: true
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
     var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
     var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
     console.assert(typeof url == 'string', 'tab.url should be a string');
 
     callback(url);
   });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
+/**
+ * Extract domain from URL. Example: http://www.google.com -> google.com
+ *
+ * @param {string}
+ */
 function extractDomain(url) {
     var domain;
     //find & remove protocol (http, ftp, etc.) and get domain
@@ -62,6 +44,11 @@ function extractDomain(url) {
     return domain;
 }
 
+/**
+ * Generate password using Master Password.
+ *
+ * @param {string}
+ */
 function generatePassword(template) {
 
   var full_name = "Adam Becker Chillman";
@@ -82,29 +69,18 @@ function generatePassword(template) {
   });
 }
 
-var PIN     = "pin";
-var SHORT   = "short";
-var BASIC   = "basic";
-var MEDIUM  = "medium";
-var LONG    = "long";
-var MAXIMUM = "maximum";
-var NAME    = "name";
-var PHRASE  = "phrase";
+var templates = [];
+templates.PIN     = "pin";
+templates.SHORT   = "short";
+templates.BASIC   = "basic";
+templates.MEDIUM  = "medium";
+templates.LONG    = "long";
+templates.MAXIMUM = "maximum";
+templates.NAME    = "name";
+templates.PHRASE  = "phrase";
 
-var templates = {
-  PIN,
-  SHORT,
-  BASIC,
-  MEDIUM,
-  LONG,
-  MAXIMUM,
-  NAME,
-  PHRASE
-}
-
-window.onload = function () {
-  var moo = document.getElementById("asdf");
-  moo.addEventListener("click", function() {
-    generatePassword(LONG);
-  }, false);
-};
+$(document).ready(function () {
+    $("#generate_password").click(function() {
+        generatePassword(templates.LONG);
+    });
+});
